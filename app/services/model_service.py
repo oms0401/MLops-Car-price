44a0d655c7f5a4bb98ed216b7301a6395b06d524
Om Manoj Sharma <oms0401vns@gmail.com>
2025-10-06 21:10:57 +0530

    prediction endpoint and logic added

---
+++ app/services/model_service.py
@@ -0,0 +1,17 @@
+import joblib
+import pandas as pd
+from app.core.config import setting
+from app.cache.redis_cache import get_cached_prediction,set_cached_prediction
+
+model =joblib.load(setting.MODEL_PATH)
+
+def predict_car_price(data: dict):
+    cached_key= " ".join([str(value) for value in data.values()])
+    cached=get_cached_prediction(cached_key)
+    if cached:
+        return cached
+    input_data=pd.DataFrame([data])
+    prediction=model.predict(input_data)[0]
+    set_cached_prediction(cached_key,prediction)
+    return prediction
+
