ccc1b3f8340ac418bc0c333b1e589a4fbe697201
Om Manoj Sharma <oms0401vns@gmail.com>
2025-10-06 23:22:20 +0530

    monitoring and containarization is added

--- app/services/model_service.py
+++ app/services/model_service.py
@@ -6,6 +6,7 @@ from app.cache.redis_cache import get_cached_prediction,set_cached_prediction
 model =joblib.load(setting.MODEL_PATH)
 
 def predict_car_price(data: dict):
+    #using the whole row as the key in order to save the cache value 
     cached_key= " ".join([str(value) for value in data.values()])
     cached=get_cached_prediction(cached_key)
     if cached:
